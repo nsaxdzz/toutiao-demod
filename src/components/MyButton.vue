23ac836b93755b6dc83b02b2c5e4a49033d79de5
张大伟 <nsaxdzz@163.com>
2021-11-09 16:37:48 +0800

    toutiao

---
+++ src/components/MyButton.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class='btn'
+       @click="btn"
+        :class="{
+            danger:type === 'danger',
+            primary:type === 'primary'
+            }"
+       >
+     
+          <slot>按钮</slot>
+  </div>
+</template>
+
+<script>
+export default {
+    props:{
+         type:String,
+    },
+    methods: {
+        btn(e){
+            this.$emit('click',e)
+            console.log('你点击了按钮')
+        }
+    }
+}
+</script>
+
+<style lang="less" scoped>
+.btn{
+    width: 318px;
+    height: 48px;
+    display: flex;
+    border-radius: 24px;
+    justify-content: center;
+    align-items: center;
+    color:#ccc;
+    font-size: 20px;
+    background-color: #666;
+    margin: 10px 0;
+
+}
+.danger{
+    background-color: red;
+    color: #fff;
+}
+.primary{
+    background-color: green;
+    color: rgb(216, 216, 216);
+}
+</style>
---
+++ src/views/Login/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+      登入页
+    <MyButton @click="loginFn"></MyButton>
+    <MyButton  @click="loginFn"  type='primary'>登入</MyButton>
+    <MyButton @click="loginFn" type='danger'>注册</MyButton>
+  </div>
+</template>
+
+<script>
+import MyButton from'@/components/MyButton.vue'
+export default {
+  components: {
+      MyButton,
+  },
+  methods: {
+      loginFn(e){
+          console.log(e)
+      }
+  }
+}
+</script>
+
+<style>
+
+</style>
